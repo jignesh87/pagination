8f0797a13e223bdd992dc3e6d5649b2a598c1614
Jignesh Makwana <j.mca25@gmail.com>
2018-10-05 00:54:23 +0530

    ii

---
+++ resources/js/app.js
@@ -0,0 +1,47 @@
+
+/**
+ * First we will load all of this project's JavaScript dependencies which
+ * includes Vue and other libraries. It is a great starting point when
+ * building robust, powerful web applications using Vue and Laravel.
+ */
+
+require('./bootstrap');
+
+window.Vue = require('vue');
+
+/**
+ * Next, we will create a fresh Vue application instance and attach it to
+ * the page. Then, you may begin adding components to this application
+ * or customize the JavaScript scaffolding to fit your unique needs.
+ */
+
+Vue.component('example-component', require('./components/ExampleComponent.vue'));
+Vue.component('pagination', require('./components/PaginationComponent.vue'));
+
+const app = new Vue({
+    el: '#app',
+    data: {
+        posts: {},
+        pagination: {
+            'current_page': 1
+        }
+    },
+
+    methods: {
+        fetchPosts() {
+            axios.get('posts?page=' + this.pagination.current_page)
+                .then(response => {
+                   // console.log(response.data.data);
+                    this.posts = response.data.data.data;
+                    this.pagination = response.data.pagination;
+                })
+                .catch(error => {
+                    console.log(error.response.data);
+                });
+        }
+    },
+
+    mounted() {
+        this.fetchPosts();
+    }
+});
